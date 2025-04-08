9eab9c01f1d65e51b80b11ae4b9b6914d787ebc1
fepo <fedortsovapoli@gmail.com>
2025-04-08 21:07:08 +0300

    update library

--- Primer.py
+++ Primer.py
@@ -1,3 +1,8 @@
 from pybrain3.tools.shortcuts import buildNetwork
 
 net = buildNetwork(2, 3, 1) # вход, скрытый слой, выход
+
+y = net.activate([2, 1])
+print("y = ", y)
+
+#TODO Исправить библиотеку!!!
