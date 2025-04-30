67788df35bd1dcf54dfb8a1fd037e1bcc989746c
fepo <fedortsovapoli@gmail.com>
2025-04-30 17:31:14 +0300

    add train and analyzis in Net_Fishing.py

--- Net_Fishing.py
+++ Net_Fishing.py
@@ -9,5 +9,21 @@ ds = SupervisedDataSet(4, 1)
 # Скорость ветра, Перепад давления, Облачность, Перепад температуры, Активность рыбы(ответ)
 ds.addSample([2, 3, 80, 1], [5])
 
-#TODO Дописать обучающий набор данных
-#TODO Дописать формирование структуры нейроной сети
+ds = SupervisedDataSet(4, 1)
+ds.addSample([2, 3, 80, 1], [5])
+ds.addSample([5, 5, 50, 2], [4])
+ds.addSample([10, 7, 40, 3], [3])
+ds.addSample([15, 9, 20, 4], [2])
+ds.addSample([20, 11, 10, 5], [1])
+
+net = buildNetwork(4, 3, 1, bias=True)
+
+trainer = BackpropTrainer(net, dataset=ds, momentum=0.1, learningrate=0.01, verbose=True, weightdecay=0.01)
+trnerr, valerr = trainer.trainUntilConvergence()
+
+plt.plot(trnerr, 'b',  valerr, 'r')
+plt.savefig("Net_FishingAnalyzis.png")
+
+fileObject = open('Net_Fishing.txt', 'wb')
+pickle.dump(net, fileObject)
+fileObject.close()
